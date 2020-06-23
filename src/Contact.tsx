8a0722e7031977dba079c7e66bf74280e6d5f572
Alex <alittle1092@gmail.com>
2020-06-23 15:23:15 +1000

    first

---
+++ src/Contact.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default () => {
+  return (
+    <>
+      <section id="contact">
+        <h2>Contact Details</h2>
+        <p>
+          <b>Phone:</b> <i>+12345678</i>
+        </p>
+        <p>
+          <b>Email:</b> <i>thatsme@test.com</i>
+        </p>
+        <p>
+          <b>Twitter:</b> <i>#superfun</i>
+        </p>
+      </section>
+    </>
+  );
+};
